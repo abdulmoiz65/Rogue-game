db8c4faaa4667cc3bc1c52635963d792f9bc7ad4
moiz <pc16786abdulmoiz@gmail.com>
2024-05-29 23:53:38 +0500

    add the black square of the game

--- src/react_rogue.jsx
+++ src/react_rogue.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React , {useRef , useEffect} from 'react';
 
 const Rogue = ({ width, height, tilesize }) => {
- 
-  return (
+
+const canvasRef = useRef();
+useEffect( () =>{
+    console.log("draw a canvas");
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.clearRect(0,0,width * tilesize ,height * tilesize); 
+    ctx.fillStyle="black";
+    ctx.fillRect(12,22,16,16,);
+})  
+
+return (
     <canvas
+      ref={canvasRef}
       width={width * tilesize}
       height={height * tilesize}
       style={{border : '1px solid'}}
