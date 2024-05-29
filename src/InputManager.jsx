873657f384bf51eeb8c60b62ea56e61b54d60f82
moiz <pc16786abdulmoiz@gmail.com>
2024-05-30 00:24:45 +0500

    square is moving ->bug� 'but not in rightside'

---
+++ src/InputManager.jsx
@@ -0,0 +1,41 @@
+class InputManager {
+    observer = [];
+
+    subscribe(fn){
+        this.observer.push(fn)
+    }
+    unsubscribe(fn){
+        this.observer = this.observer.filter(subscribe => subscribe !== fn);
+        }
+
+    broadcast(action , data){
+        this.observer.forEach(subscribe => subscribe(action,data));
+    }   
+    
+    handlekeys = e =>{
+        e.preventDefault();
+        switch(e.keyCode){
+            case 37: this.broadcast('move', {x:1 , y:0});
+            break;
+            case 38: this.broadcast('move' , {x:0 , y:-1});
+            break;
+            case 39: this.broadcast('move',{x:1 , y:0});
+            break;
+            case 40: this.broadcast('move',{x:0 , y:1});
+            break;
+            default:
+                break;
+        }
+    }
+
+    bindkeys(){
+        document.addEventListener('keydown' , this.handlekeys);
+    }
+    unbindkeys(){
+        document.removeEventListener('keydown',this.handlekeys);
+    }
+
+    
+    
+}
+export default InputManager ;
--- src/react_rogue.jsx
+++ src/react_rogue.jsx
@@ -1,14 +1,39 @@
-import React , {useRef , useEffect} from 'react';
+import React , {useRef , useEffect ,useState} from 'react';
+import InputManager from './InputManager';
 
 const Rogue = ({ width, height, tilesize }) => {
 
 const canvasRef = useRef();
+let inputManager = new InputManager();
+const [player,setplayer] = useState({x:64 , y:128});
+
+const handleInput = (action,data)=> {
+    console.log(`handle input : $(action) : ${JSON.stringify(data)}`);
+    let newplayer = {...player};
+    newplayer.x += data.x * tilesize;
+    newplayer.y += data.y * tilesize;
+    setplayer(newplayer);
+}
+
+
+
+useEffect(()=>{
+    console.log("bindkeys");
+    inputManager.bindkeys();
+    inputManager.subscribe(handleInput);
+    return()=>
+        {
+            inputManager.unbindkeys();
+            inputManager.unsubscribe(handleInput);
+        }
+})
+
 useEffect( () =>{
     console.log("draw a canvas");
     const ctx = canvasRef.current.getContext('2d');
     ctx.clearRect(0,0,width * tilesize ,height * tilesize); 
     ctx.fillStyle="black";
-    ctx.fillRect(12,22,16,16,);
+    ctx.fillRect(player.x,player.y,16,16,);
 })  
 
 return (
